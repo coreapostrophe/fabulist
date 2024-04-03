eeec677fc23ec4b034569340232891623aea3fb1
Core <daverencordero@gmail.com>
2024-04-04 05:06:50 +0800

    test(fabulist_lang): replaced macros with helper functions

--- packages/fabulist_lang/src/parser.rs
+++ packages/fabulist_lang/src/parser.rs
@@ -1,5 +1,7 @@
 use pest::{error::LineColLocation, iterators::Pairs, Parser, RuleType};
 
+mod test_helpers;
+
 #[derive(pest_derive::Parser)]
 #[grammar = "./fabulist.pest"]
 struct GrammarParser;
@@ -33,225 +35,114 @@ impl FabulistParser {
 
 #[cfg(test)]
 mod parser_tests {
-    use pest::Parser;
-
-    use crate::parser::{GrammarParser, Rule};
-
-    macro_rules! assert_primitive {
-        ($rule:expr, $source:expr) => {
-            let result = GrammarParser::parse($rule, $source);
-            assert!(result.is_ok());
-        };
-    }
-
-    macro_rules! assert_primary {
-        ($rule:expr, $source:expr) => {
-            let result = GrammarParser::parse(Rule::primary, $source);
-            assert_eq!(result.clone().unwrap().next().unwrap().as_rule(), $rule);
-        };
-    }
-
-    macro_rules! assert_call {
-        ($source:expr, $callee:expr, $($arg:expr),*) => {
-            let result = GrammarParser::parse(Rule::call, $source);
-            assert_eq!(
-                result.clone().unwrap().next().unwrap().as_rule(),
-                Rule::call
-            );
-            assert_eq!(
-                result
-                    .clone()
-                    .unwrap()
-                    .next()
-                    .unwrap()
-                    .into_inner()
-                    .find(|p| p.as_node_tag().unwrap() == "callee")
-                    .unwrap()
-                    .as_str(),
-                $callee
-            );
-            assert_eq!(
-                result
-                    .clone()
-                    .unwrap()
-                    .next()
-                    .unwrap()
-                    .into_inner()
-                    .find(|p| p.as_rule() == Rule::argument_body)
-                    .unwrap()
-                    .into_inner()
-                    .next()
-                    .unwrap()
-                    .into_inner()
-                    .map(|p| p.as_str())
-                    .collect::<Vec<_>>(),
-                $($arg)*
-            );
-        };
-    }
-
-    macro_rules! assert_unary {
-        ($source:expr, $operator:expr, $right:expr) => {
-            let result = GrammarParser::parse(Rule::unary, $source);
-            assert_eq!(
-                result
-                    .clone()
-                    .unwrap()
-                    .next()
-                    .unwrap()
-                    .into_inner()
-                    .find_tagged("operator")
-                    .next()
-                    .unwrap()
-                    .as_str(),
-                $operator
-            );
-            assert_eq!(
-                result
-                    .unwrap()
-                    .next()
-                    .unwrap()
-                    .into_inner()
-                    .find_tagged("right")
-                    .next()
-                    .unwrap()
-                    .as_str(),
-                $right
-            );
-        };
-    }
-
-    macro_rules! assert_binomial_operation {
-        ($rule:expr, $source:expr, ($left:expr, $operator:expr, $right:expr)) => {
-            let result = GrammarParser::parse($rule, $source);
-            println!("{:#?}", result.clone());
-            assert_eq!(
-                result
-                    .clone()
-                    .unwrap()
-                    .next()
-                    .unwrap()
-                    .into_inner()
-                    .find_tagged("left")
-                    .next()
-                    .unwrap()
-                    .as_str(),
-                $left
-            );
-            assert_eq!(
-                result
-                    .clone()
-                    .unwrap()
-                    .next()
-                    .unwrap()
-                    .into_inner()
-                    .find_tagged("operator")
-                    .next()
-                    .unwrap()
-                    .as_str(),
-                $operator
-            );
-            assert_eq!(
-                result
-                    .unwrap()
-                    .next()
-                    .unwrap()
-                    .into_inner()
-                    .find_tagged("right")
-                    .next()
-                    .unwrap()
-                    .as_str(),
-                $right
-            );
-        };
-    }
+    use super::{test_helpers::*, Rule};
 
     #[test]
     fn parses_primitives() {
-        assert_primitive!(Rule::number, "5");
-        assert_primitive!(Rule::string, "\"sample string\"");
-        assert_primitive!(Rule::boolean, "true");
-        assert_primitive!(Rule::boolean, "false");
-        assert_primitive!(Rule::identifier, "sample_identifier");
+        assert_primitive(Rule::number, "5");
+        assert_primitive(Rule::string, "\"sample string\"");
+        assert_primitive(Rule::boolean, "true");
+        assert_primitive(Rule::boolean, "false");
+        assert_primitive(Rule::identifier, "sample_identifier");
     }
 
     #[test]
     fn parses_primaries() {
-        assert_primary!(Rule::number, "5");
-        assert_primary!(Rule::string, "\"sample string\"");
-        assert_primary!(Rule::boolean, "true");
-        assert_primary!(Rule::boolean, "false");
-        assert_primary!(Rule::identifier, "sample_identifier");
-        assert_primary!(Rule::grouping, "(5 + 5)");
-        assert_primary!(Rule::none, "none");
-        assert_primary!(Rule::object, "{\"key\":\"value\"}");
-        assert_primary!(Rule::lambda_function, "(arg1, arg2)=>{ 5; }");
+        assert_primary(Rule::number, "5");
+        assert_primary(Rule::string, "\"sample string\"");
+        assert_primary(Rule::boolean, "true");
+        assert_primary(Rule::boolean, "false");
+        assert_primary(Rule::identifier, "sample_identifier");
+        assert_primary(Rule::grouping, "(5 + 5)");
+        assert_primary(Rule::none, "none");
+        assert_primary(Rule::object, "{\"key\":\"value\"}");
+        assert_primary(Rule::lambda_function, "(arg1, arg2)=>{ 5; }");
+    }
+
+    #[test]
+    fn parses_lambda_functions() {
+        assert_lambda_function("() => {}", vec![], vec![]);
+        assert_lambda_function(
+            "(param1, param2) => {     }",
+            vec!["param1", "param2"],
+            vec![],
+        );
+        assert_lambda_function(
+            "() =>{     statement1; statement2;}",
+            vec![],
+            vec!["statement1", "statement2"],
+        );
+        assert_lambda_function(
+            "(param1,param2)=>{param1;param2;}",
+            vec!["param1", "param2"],
+            vec!["param1;", "param2;"],
+        );
     }
 
     #[test]
     fn parses_calls() {
-        assert_call!("sum(addend1, addend2)", "sum", ["addend1", "addend2"]);
-        assert_call!(
+        assert_call("sum(addend1, addend2)", "sum", vec!["addend1", "addend2"]);
+        assert_call(
             "subtraction(minuend, subtrahend)",
             "subtraction",
-            ["minuend", "subtrahend"]
+            vec!["minuend", "subtrahend"],
         );
-        assert_call!(
+        assert_call(
             "division(dividend, divisor)",
             "division",
-            ["dividend", "divisor"]
+            vec!["dividend", "divisor"],
         );
-        assert_call!(
+        assert_call(
             "multiplication(multiplicand, multiplier)",
             "multiplication",
-            ["multiplicand", "multiplier"]
+            vec!["multiplicand", "multiplier"],
         );
     }
 
     #[test]
     fn parses_unary() {
-        assert_unary!("!5", "!", "5");
-        assert_unary!("-5", "-", "5");
-        assert_unary!("-(5 + 5)", "-", "(5 + 5)");
-        assert_unary!("-(  2 > false )", "-", "(  2 > false )");
+        assert_unary("!5", "!", "5");
+        assert_unary("-5", "-", "5");
+        assert_unary("-(5 + 5)", "-", "(5 + 5)");
+        assert_unary("-(  2 > false )", "-", "(  2 > false )");
     }
 
     #[test]
     fn parses_factor() {
-        assert_binomial_operation!(Rule::factor, "5 /5", ("5 ", "/", "5"));
-        assert_binomial_operation!(Rule::factor, "5 *(1+2)", ("5 ", "*", "(1+2)"));
-        assert_binomial_operation!(Rule::factor, "true /5", ("true ", "/", "5"));
+        assert_binomial_operation(Rule::factor, "5 /5", ["5 ", "/", "5"]);
+        assert_binomial_operation(Rule::factor, "5 *(1+2)", ["5 ", "*", "(1+2)"]);
+        assert_binomial_operation(Rule::factor, "true /5", ["true ", "/", "5"]);
     }
 
     #[test]
     fn parses_term() {
-        assert_binomial_operation!(Rule::term, "1 +7", ("1 ", "+", "7"));
-        assert_binomial_operation!(Rule::term, "6 -   9", ("6 ", "-", "9"));
-        assert_binomial_operation!(Rule::term, "3 + (5)", ("3 ", "+", "(5)"));
-        assert_binomial_operation!(Rule::term, "5-3", ("5", "-", "3"));
+        assert_binomial_operation(Rule::term, "1 +7", ["1 ", "+", "7"]);
+        assert_binomial_operation(Rule::term, "6 -   9", ["6 ", "-", "9"]);
+        assert_binomial_operation(Rule::term, "3 + (5)", ["3 ", "+", "(5)"]);
+        assert_binomial_operation(Rule::term, "5-3", ["5", "-", "3"]);
     }
 
     #[test]
     fn parses_comparison() {
-        assert_binomial_operation!(Rule::comparison, "6 < 7", ("6 ", "<", "7"));
-        assert_binomial_operation!(Rule::comparison, "8<= (7)(true)", ("8", "<=", "(7)(true)"));
-        assert_binomial_operation!(Rule::comparison, "1    >2", ("1    ", ">", "2"));
-        assert_binomial_operation!(Rule::comparison, "0>= (1 + 5)", ("0", ">=", "(1 + 5)"));
+        assert_binomial_operation(Rule::comparison, "6 < 7", ["6 ", "<", "7"]);
+        assert_binomial_operation(Rule::comparison, "8<= (7)(true)", ["8", "<=", "(7)(true)"]);
+        assert_binomial_operation(Rule::comparison, "1    >2", ["1    ", ">", "2"]);
+        assert_binomial_operation(Rule::comparison, "0>= (1 + 5)", ["0", ">=", "(1 + 5)"]);
     }
 
     #[test]
     fn parses_equality() {
-        assert_binomial_operation!(Rule::equality, "2 == 9", ("2 ", "==", "9"));
-        assert_binomial_operation!(Rule::equality, "6    != false", ("6    ", "!=", "false"));
-        assert_binomial_operation!(Rule::equality, "4==(i)=>{i;}", ("4", "==", "(i)=>{i;}"));
-        assert_binomial_operation!(Rule::equality, "7 != true", ("7 ", "!=", "true"));
+        assert_binomial_operation(Rule::equality, "2 == 9", ["2 ", "==", "9"]);
+        assert_binomial_operation(Rule::equality, "6    != false", ["6    ", "!=", "false"]);
+        assert_binomial_operation(Rule::equality, "4==(i)=>{i;}", ["4", "==", "(i)=>{i;}"]);
+        assert_binomial_operation(Rule::equality, "7 != true", ["7 ", "!=", "true"]);
     }
 
     #[test]
     fn parses_logical() {
-        assert_binomial_operation!(Rule::logical, "1&&1", ("1", "&&", "1"));
-        assert_binomial_operation!(Rule::logical, "yo ||   yo1", ("yo ", "||", "yo1"));
-        assert_binomial_operation!(Rule::logical, "true && false", ("true ", "&&", "false"));
-        assert_binomial_operation!(Rule::logical, "hello|| test()", ("hello", "||", "test()"));
+        assert_binomial_operation(Rule::logical, "1&&1", ["1", "&&", "1"]);
+        assert_binomial_operation(Rule::logical, "yo ||   yo1", ["yo ", "||", "yo1"]);
+        assert_binomial_operation(Rule::logical, "true && false", ["true ", "&&", "false"]);
+        assert_binomial_operation(Rule::logical, "hello|| test()", ["hello", "||", "test()"]);
     }
 }
---
+++ packages/fabulist_lang/src/parser/test_helpers.rs
@@ -0,0 +1,139 @@
+#![allow(dead_code)]
+
+use super::{GrammarParser, Rule};
+use pest::Parser;
+
+pub fn assert_primitive(rule: Rule, source: &str) {
+    let result = GrammarParser::parse(rule, source);
+    assert!(result.is_ok());
+}
+
+pub fn assert_primary(rule: Rule, source: &str) {
+    let result = GrammarParser::parse(Rule::primary, source);
+    let mut result = result.expect("Failed to parse source.");
+    let primary = result.next().expect("Failed to extract primary pair.");
+    let primary_rule = primary.as_rule();
+    assert_eq!(primary_rule, rule);
+}
+
+pub fn assert_lambda_function(
+    source: &str,
+    parameter_assertions: Vec<&str>,
+    statement_assertions: Vec<&str>,
+) {
+    let result = GrammarParser::parse(Rule::lambda_function, source);
+    let mut result = result.expect("Failed to parse source.");
+    let lambda_function = result
+        .next()
+        .expect("Failed to extract lambda_function pair.");
+    let parameter_body = lambda_function
+        .clone()
+        .into_inner()
+        .find(|p| p.as_rule() == Rule::parameter_body)
+        .expect("Failed to extract parameter_body pair.");
+    let parameters = parameter_body
+        .clone()
+        .into_inner()
+        .find(|p| p.as_rule() == Rule::parameters);
+    match parameters {
+        Some(parameters) => {
+            let parameter_array: Vec<_> = parameters.into_inner().map(|p| p.as_str()).collect();
+            assert_eq!(parameter_array, parameter_assertions);
+        }
+        _ => (),
+    }
+    let statement_body = lambda_function
+        .clone()
+        .into_inner()
+        .find(|p| p.as_rule() == Rule::statement_body)
+        .expect("Failed to extract statement_body pair.");
+    let statements = statement_body
+        .clone()
+        .into_inner()
+        .find(|p| p.as_rule() == Rule::action_statement);
+    match statements {
+        Some(statements) => {
+            let statement_array: Vec<_> = statements.into_inner().map(|p| p.as_str()).collect();
+            assert_eq!(statement_array, statement_assertions);
+        }
+        _ => (),
+    }
+}
+
+pub fn assert_call(source: &str, callee_assertion: &str, argument_assertions: Vec<&str>) {
+    let result = GrammarParser::parse(Rule::call, source);
+    let mut result = result.expect("Failed to parse source.");
+    let call = result.next().expect("Failed to extract call pair.");
+    let callee = call
+        .clone()
+        .into_inner()
+        .find_tagged("callee")
+        .next()
+        .expect("Failed to extract callee pair.");
+    assert_eq!(callee.as_str(), callee_assertion);
+    let argument_body = call
+        .into_inner()
+        .find(|p| p.as_rule() == Rule::argument_body)
+        .expect("Failed to extract argument_body pair.");
+    let arguments = argument_body
+        .clone()
+        .into_inner()
+        .find(|p| p.as_rule() == Rule::arguments);
+    match arguments {
+        Some(arguments) => {
+            let argument_array: Vec<_> = arguments.into_inner().map(|p| p.as_str()).collect();
+            assert_eq!(argument_array, argument_assertions);
+        }
+        _ => (),
+    }
+}
+
+pub fn assert_unary(source: &str, operator_assertion: &str, right_assertion: &str) {
+    let result = GrammarParser::parse(Rule::unary, source);
+    let mut result = result.expect("Failed to parse source.");
+    let unary = result.next().expect("Failed to extract unary pair.");
+    let operator = unary
+        .clone()
+        .into_inner()
+        .find_tagged("operator")
+        .next()
+        .expect("Failed to extract operator pair.");
+    assert_eq!(operator.as_str(), operator_assertion);
+    let right = unary
+        .clone()
+        .into_inner()
+        .find_tagged("right")
+        .next()
+        .expect("Failed to extract right pair.");
+    assert_eq!(right.as_str(), right_assertion);
+}
+
+pub fn assert_binomial_operation(rule: Rule, source: &str, assertions: [&str; 3]) {
+    let [left_assertion, operator_assertion, right_assertion] = assertions;
+    let result = GrammarParser::parse(rule, source);
+    let mut result = result.expect("Failed to parse source.");
+    let binomial_operation = result
+        .next()
+        .expect("Failed to extract binomial_operation pair.");
+    let left = binomial_operation
+        .clone()
+        .into_inner()
+        .find_tagged("left")
+        .next()
+        .expect("Failed to extract left pair.");
+    assert_eq!(left.as_str(), left_assertion);
+    let operator = binomial_operation
+        .clone()
+        .into_inner()
+        .find_tagged("operator")
+        .next()
+        .expect("Failed to extract operator pair.");
+    assert_eq!(operator.as_str(), operator_assertion);
+    let right = binomial_operation
+        .clone()
+        .into_inner()
+        .find_tagged("right")
+        .next()
+        .expect("Failed to extract right pair.");
+    assert_eq!(right.as_str(), right_assertion);
+}
