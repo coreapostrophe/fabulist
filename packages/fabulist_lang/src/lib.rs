f59aa87330668388469a45fa586d2411d8f8aeb2
Core <daverencordero@gmail.com>
2024-04-03 12:06:00 +0800

    test(fabulist_lang): added assertions for calls, unary and factor expressions

--- packages/fabulist_lang/src/lib.rs
+++ packages/fabulist_lang/src/lib.rs
@@ -37,61 +37,186 @@ mod parser_tests {
 
     use crate::{GrammarParser, Rule};
 
+    macro_rules! assert_primitive {
+        ($rule:expr, $source:expr) => {
+            let result = GrammarParser::parse($rule, $source);
+            assert!(result.is_ok());
+        };
+    }
+
+    macro_rules! assert_primary {
+        ($rule:expr, $source:expr) => {
+            let result = GrammarParser::parse(Rule::primary, $source);
+            assert_eq!(result.clone().unwrap().next().unwrap().as_rule(), $rule);
+        };
+    }
+
+    macro_rules! assert_call {
+        ($source:expr, $callee:expr, $($arg:expr),*) => {
+            let result = GrammarParser::parse(Rule::call, $source);
+            assert_eq!(
+                result.clone().unwrap().next().unwrap().as_rule(),
+                Rule::call
+            );
+            assert_eq!(
+                result
+                    .clone()
+                    .unwrap()
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .find(|p| p.as_node_tag().unwrap() == "callee")
+                    .unwrap()
+                    .as_str(),
+                $callee
+            );
+            assert_eq!(
+                result
+                    .clone()
+                    .unwrap()
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .find(|p| p.as_rule() == Rule::arguments)
+                    .unwrap()
+                    .into_inner()
+                    .map(|p| p.as_str())
+                    .collect::<Vec<_>>(),
+                $($arg)*
+            );
+        };
+    }
+
+    macro_rules! assert_unary {
+        ($source:expr, $operator:expr, $right:expr) => {
+            let result = GrammarParser::parse(Rule::unary, $source);
+            assert_eq!(
+                result
+                    .clone()
+                    .unwrap()
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .find_tagged("operator")
+                    .next()
+                    .unwrap()
+                    .as_str(),
+                $operator
+            );
+            assert_eq!(
+                result
+                    .unwrap()
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .find_tagged("right")
+                    .next()
+                    .unwrap()
+                    .as_str(),
+                $right
+            );
+        };
+    }
+
+    macro_rules! assert_factor {
+        ($source:expr, $left:expr, $operator:expr, $right:expr) => {
+            let result = GrammarParser::parse(Rule::factor, $source);
+            assert_eq!(
+                result
+                    .clone()
+                    .unwrap()
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .find_tagged("left")
+                    .next()
+                    .unwrap()
+                    .as_str(),
+                $left
+            );
+            assert_eq!(
+                result
+                    .clone()
+                    .unwrap()
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .find_tagged("operator")
+                    .next()
+                    .unwrap()
+                    .as_str(),
+                $operator
+            );
+            assert_eq!(
+                result
+                    .clone()
+                    .unwrap()
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .find_tagged("right")
+                    .next()
+                    .unwrap()
+                    .as_str(),
+                $right
+            );
+        };
+    }
+
     #[test]
     fn parses_primitives() {
-        let result = GrammarParser::parse(Rule::number, "5");
-        assert!(result.is_ok());
-        let result = GrammarParser::parse(Rule::string, "\"sample string\"");
-        assert!(result.is_ok());
-        let result = GrammarParser::parse(Rule::boolean, "true");
-        assert!(result.is_ok());
-        let result = GrammarParser::parse(Rule::boolean, "false");
-        assert!(result.is_ok());
-        let result = GrammarParser::parse(Rule::identifier, "sample_identifier");
-        assert!(result.is_ok());
+        assert_primitive!(Rule::number, "5");
+        assert_primitive!(Rule::string, "\"sample string\"");
+        assert_primitive!(Rule::boolean, "true");
+        assert_primitive!(Rule::boolean, "false");
+        assert_primitive!(Rule::identifier, "sample_identifier");
     }
 
     #[test]
     fn parses_primaries() {
-        let result = GrammarParser::parse(Rule::primary, "5");
-        assert_eq!(
-            result.clone().unwrap().next().unwrap().as_rule(),
-            Rule::number
+        assert_primary!(Rule::number, "5");
+        assert_primary!(Rule::string, "\"sample string\"");
+        assert_primary!(Rule::boolean, "true");
+        assert_primary!(Rule::boolean, "false");
+        assert_primary!(Rule::identifier, "sample_identifier");
+        assert_primary!(Rule::grouping, "(5 + 5)");
+        assert_primary!(Rule::none, "none");
+        assert_primary!(Rule::object, "{\"key\":\"value\"}");
+        assert_primary!(Rule::lambda_function, "(arg1, arg2)=>{ 5; }");
+    }
+
+    #[test]
+    fn parses_calls() {
+        assert_call!("sum(addend1, addend2)", "sum", ["addend1", "addend2"]);
+        assert_call!(
+            "subtraction(minuend, subtrahend)",
+            "subtraction",
+            ["minuend", "subtrahend"]
         );
-        let result = GrammarParser::parse(Rule::primary, "\"sample string\"");
-        assert_eq!(
-            result.clone().unwrap().next().unwrap().as_rule(),
-            Rule::string
+        assert_call!(
+            "division(dividend, divisor)",
+            "division",
+            ["dividend", "divisor"]
         );
-        let result = GrammarParser::parse(Rule::primary, "true");
-        assert_eq!(
-            result.clone().unwrap().next().unwrap().as_rule(),
-            Rule::boolean
+        assert_call!(
+            "multiplication(multiplicand, multiplier)",
+            "multiplication",
+            ["multiplicand", "multiplier"]
         );
-        let result = GrammarParser::parse(Rule::primary, "sample_identifier");
-        assert_eq!(
-            result.clone().unwrap().next().unwrap().as_rule(),
-            Rule::identifier
-        );
-        let result = GrammarParser::parse(Rule::primary, "(5 + 5)");
-        assert_eq!(
-            result.clone().unwrap().next().unwrap().as_rule(),
-            Rule::grouping
-        );
-        let result = GrammarParser::parse(Rule::primary, "none");
-        assert_eq!(
-            result.clone().unwrap().next().unwrap().as_rule(),
-            Rule::none
-        );
-        let result = GrammarParser::parse(Rule::primary, "{\"key\":\"value\"}");
-        assert_eq!(
-            result.clone().unwrap().next().unwrap().as_rule(),
-            Rule::object
-        );
-        let result = GrammarParser::parse(Rule::primary, "(arg1, arg2)=>{ 5; }");
-        assert_eq!(
-            result.clone().unwrap().next().unwrap().as_rule(),
-            Rule::lambda_function
-        );
+    }
+
+    #[test]
+    fn parses_unary() {
+        assert_unary!("!5", "!", "5");
+        assert_unary!("-5", "-", "5");
+        assert_unary!("-(5 + 5)", "-", "(5 + 5)");
+        assert_unary!("-(  2 > false )", "-", "(  2 > false )");
+    }
+
+    #[test]
+    fn parses_factor() {
+        assert_factor!("5 /5", "5 ", "/", "5");
+        assert_factor!("5 *(1+2)", "5 ", "*", "(1+2)");
+        assert_factor!("true /5", "true ", "/", "5");
     }
 }
