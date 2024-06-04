f27543d85945719fcff15c6e76a7ef731d55900c
Core <daverencordero@gmail.com>
2024-06-04 18:49:21 +0800

    chore(lang): resolved linting issues

--- packages/fabulist_lang/src/ast/expr/overrides.rs
+++ packages/fabulist_lang/src/ast/expr/overrides.rs
@@ -3,7 +3,7 @@ use crate::{error::Error, parser::Rule};
 use super::models::Literal;
 
 impl Literal {
-    pub fn to_f32(&self) -> Result<f32, pest::error::Error<Rule>> {
+    pub fn to_f32(&self) -> Result<f32, Box<pest::error::Error<Rule>>> {
         match self {
             Literal::Boolean(boolean) => Ok(if boolean.value { 1.0 } else { 0.0 }),
             Literal::Number(number) => Ok(number.value),
@@ -12,7 +12,7 @@ impl Literal {
                 let span = string.span.to_owned();
                 let value = string.value.to_owned();
                 value.parse::<f32>().map_err(|_| {
-                    Error::map_custom_error(span, format!("Unable to parse `{}` to `f32`", value))
+                    Box::new(Error::map_custom_error(span, format!("Unable to parse `{}` to `f32`", value)))
                 })
             }
         }
