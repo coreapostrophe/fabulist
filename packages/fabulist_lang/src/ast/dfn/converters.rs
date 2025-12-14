da826eaacfa00bd0fba8e08e6297e04f2372dae1
Core <daverencordero@gmail.com>
2025-12-14 12:11:51 +0800

    test(lang): moved model tests to converters

--- packages/fabulist_lang/src/ast/dfn/converters.rs
+++ packages/fabulist_lang/src/ast/dfn/converters.rs
@@ -110,3 +110,30 @@ impl TryFrom<Pair<'_, Rule>> for ParameterBodyDfn {
         }
     }
 }
+
+#[cfg(test)]
+mod dfn_converters_tests {
+    use crate::{ast::AstTestHelper, parser::Rule};
+
+    use super::*;
+
+    #[test]
+    pub fn parses_parameter_body() {
+        let test_helper =
+            AstTestHelper::<ParameterBodyDfn>::new(Rule::parameter_body, "ParameterBodyDfn");
+        test_helper.assert_parse(r#"(param1, param2, param3)"#);
+    }
+
+    #[test]
+    pub fn parses_argument_body() {
+        let test_helper =
+            AstTestHelper::<ArgumentBodyDfn>::new(Rule::argument_body, "ArgumentBodyDfn");
+        test_helper.assert_parse(r#"("string", 5, true)"#);
+    }
+
+    #[test]
+    fn parses_object() {
+        let test_helper = AstTestHelper::<ObjectDfn>::new(Rule::object, "ObjectDfn");
+        test_helper.assert_parse(r#"{"boolean": false, "number": 10}"#);
+    }
+}
--- packages/fabulist_lang/src/ast/stmt/converters.rs
+++ packages/fabulist_lang/src/ast/stmt/converters.rs
@@ -170,3 +170,41 @@ impl TryFrom<Pair<'_, Rule>> for GotoStmt {
         })
     }
 }
+
+#[cfg(test)]
+mod stmt_converters_tests {
+    use crate::{ast::AstTestHelper, parser::Rule};
+
+    use super::*;
+
+    #[test]
+    fn parses_block_stmt() {
+        let test_helper = AstTestHelper::<BlockStmt>::new(Rule::block_stmt, "BlockStmt");
+        test_helper.assert_parse(
+            r#"{
+                let key = "value";
+                goto module_1::part_1;
+                if true {} else if true {} else {}
+            }"#,
+        );
+    }
+
+    #[test]
+    fn parses_if_stmt() {
+        let test_helper = AstTestHelper::<IfStmt>::new(Rule::if_stmt, "IfStmt");
+        test_helper.assert_parse("if true {}");
+        test_helper.assert_parse("if true {} else {}");
+    }
+
+    #[test]
+    fn parses_let_stmt() {
+        let test_helper = AstTestHelper::<LetStmt>::new(Rule::let_stmt, "LetStmt");
+        test_helper.assert_parse("let key = \"value\";");
+    }
+
+    #[test]
+    fn parses_goto_stmt() {
+        let test_helper = AstTestHelper::<GotoStmt>::new(Rule::goto_stmt, "GotoStmt");
+        test_helper.assert_parse("goto module_1::part_1;");
+    }
+}
